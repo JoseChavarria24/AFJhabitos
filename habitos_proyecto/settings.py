29d2606a685f4b03184e6319109a10cb947bcf02
JoseChavarria24 <azms.34@gmail.com>
2025-05-27 23:53:09 -0600

    Allowed hosts

--- habitos_proyecto/settings.py
+++ habitos_proyecto/settings.py
@@ -26,8 +26,9 @@ SECRET_KEY = 'django-insecure-nw527-m7p_a!b1vyuqx4%@!6twze^6f=x2wp+vof##e2zr!rok
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["https://afjhabitos.onrender.com","*"]
+ALLOWED_HOSTS = ['https://afjhabitos.onrender.com', 'localhost', '127.0.0.1']
 
+CSRF_TRUSTED_ORIGINS = ['https://afjhabitos.onrender.com']
 
 # Application definition
 
